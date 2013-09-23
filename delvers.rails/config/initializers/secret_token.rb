43c7b3509012deea0e3662712db53517ddea7c5b
Mike Roylance <roylance.michael@gmail.com>
2013-09-22 20:26:47 -0600

    also adding a rails project, will host through heroku

---
+++ delvers.rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DelversRails::Application.config.secret_token = '45323992a1fefdf9653d41fddba341a0b88fdbf7d65e06a0efe111619d9673e75992ca786db2aa67fada56eab339d1a811d6ad1a5f53c7cf455277206fd9352f'
